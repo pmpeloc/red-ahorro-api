2e50d2b21498a07456278b0d5f3b8774cc7d0fd4
Pablo Misael Peloc <pmpeloc@gmail.com>
2024-01-20 03:06:17 -0300

    chore: PreCommit added

---
+++ dist/types/genericErrors.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GenericErrorsMessages = exports.GenericErrorsCode = void 0;
+var GenericErrorsCode;
+(function (GenericErrorsCode) {
+    GenericErrorsCode["ERROR_500"] = "ERROR_500";
+})(GenericErrorsCode || (exports.GenericErrorsCode = GenericErrorsCode = {}));
+var GenericErrorsMessages;
+(function (GenericErrorsMessages) {
+    GenericErrorsMessages["ERROR_500"] = "Internal Server Error";
+})(GenericErrorsMessages || (exports.GenericErrorsMessages = GenericErrorsMessages = {}));
